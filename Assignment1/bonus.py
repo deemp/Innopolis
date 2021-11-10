7a1905cdb57f3b977028029ff2713a1909b1d6c4
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2021-11-11 01:26:11 +0300

    add set target joint config

--- Assignment1/bonus.py
+++ Assignment1/bonus.py
@@ -1,17 +1,23 @@
+from re import S
 from Danko_Danila_HA1 import np, get_sols, FK_solve, plt, eq
-from matplotlib.widgets import Slider, Button
+from matplotlib.widgets import Slider, Button, CheckButtons
 import matplotlib.animation as anima
 
 input = [2, 1, 3, 2, 1, 5]
 input_init = [0., 0., 0., 0., 0., 0]
-import time
+frames = 20
+
+# Use sliders to set the target joint configuration
+# Press Animate to see how the manipulator will reach the target joint config from the initial config
+# Check Many if you want to see all IK solutions at the same time
 
 class ManipulatorPlotter():
-    def __init__(self, qs, qs_0, qs_1, frames) -> None:
+    def __init__(self, qs=input_init, qs_0=input_init, qs_1=input, frames=frames) -> None:
         self.qs = qs
         self.qs_0 = qs_0
         self.qs_1 = qs_1
         self.frames = frames
+        self.many = True
         self.q_space = self.get_q_space()
         self.fig = plt.figure()
         self.plt_ax = self.fig.add_subplot(1,2,1, projection='3d')
@@ -20,9 +26,9 @@ class ManipulatorPlotter():
         self.sliders = self.add_sliders()
         self.update_with_sliders()
         self.reset_button = self.add_reset_button()
-        self.animate_button = self.get_animate_button()
-        self.animation = None
-        
+        self.animate_button = self.add_animate_button()
+        self.check_button = self.add_checkbox_many()
+                
         plt.show()
 
     def plot_qs_list(self, sols):
@@ -43,6 +49,8 @@ class ManipulatorPlotter():
 
     def update_with_sliders(self, val=None):
         new_qs = [self.sliders[i].val for i in range(6)]
+        self.qs_1 = new_qs
+        self.q_space = self.get_q_space()
         self.reset_ax()
         self.plot_qs_list(sols=get_sols(qs=new_qs))
         self.fig.canvas.draw_idle()
@@ -64,28 +72,40 @@ class ManipulatorPlotter():
 
     def reset(self, event=None):
         self.reset_ax()
+        self.animation = None
         for i in self.sliders:
             i.reset()
-        self.plot_qs_list(qs=get_sols(qs=self.qs))
+        sols = get_sols(qs=self.qs) if self.many else [self.qs]
+        self.plot_qs_list(sols=sols)
 
     def get_q_space(self):
         return np.linspace(tuple(self.qs_0), tuple(self.qs_1), self.frames)
 
-    def animate_frame(self, i, many):
+    def animate_frame(self, i):
         self.reset_ax()
-        if many == True:
+        if self.many == True:
             return self.plot_qs_list(get_sols(qs=self.q_space[i]))
         else: 
             return self.plot_qs_list([self.q_space[i]])
 
-    def animate(self, many=True, event=None):
-        self.animation = anima.FuncAnimation(fig=self.fig, func=lambda i: self.animate_frame(i=i, many=many), frames=self.frames, interval=50, blit=False)
-        return anima.FuncAnimation
+    def animate(self, event=None):
+        self.animation = anima.FuncAnimation(fig=self.fig, func=self.animate_frame, frames=self.frames, interval=50, blit=False, repeat=False)
         
-    def get_animate_button(self):
-        animate_ax = self.fig.add_subplot(8,2,16)
+    def add_animate_button(self):
+        animate_ax = self.fig.add_subplot(8,4,31)
         animate_button = Button(animate_ax, 'Animate', color='lightgoldenrodyellow')
         animate_button.on_clicked(self.animate)
         return animate_button
 
-mp = ManipulatorPlotter(qs=input_init, qs_0=input_init, qs_1=input, frames=10)
+    def update_many(self, event=None):
+        self.many = not self.many
+
+    def add_checkbox_many(self):
+        ax = self.fig.add_subplot(8,4,32)
+        label = ["Many"]
+        visibility = [True]
+        check = CheckButtons(ax, label, visibility)
+        check.on_clicked(self.update_many)
+        return check
+
+mp = ManipulatorPlotter()
