fb30ceca91fbd309bea17e6d4177b63a86a6a49c
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-01-22 18:15:21 +0300

    add 3.1

---
+++ practice_1/task3.1.py
@@ -0,0 +1,79 @@
+#%%
+from time import sleep
+from can import CANDevice, CANSocket
+import numpy as np
+
+can_bus = CANSocket()
+encoder = CANDevice(can_bus = can_bus, device_id = 0x141)
+
+encoder.command = b"\x9C"+ 7 * b"\x00"
+
+raw_state = {}
+
+def parse_sensor_data(reply):
+    """parse the raw sensor data from the CAN frame"""
+    raw_state["temp"] = reply[1]
+    raw_state["current"] = encoder.from_bytes(reply[2:4])
+    raw_state["speed"] = encoder.from_bytes(reply[4:6])
+    raw_state["encoder"] = encoder.from_bytes(reply[6:])
+    
+    return raw_state
+
+import matplotlib.pyplot as plt
+
+t = 2
+n = 1000
+dt = t/n
+dts = [dt, 2 * dt, 3 * dt]
+a = np.zeros((3,n))
+N = 2**14
+threshold = 16000
+
+try:
+    for j,k in enumerate(dts):
+        counts = 0
+        prev = 0
+        for i in range(n):
+            encoder.execute()
+            state = parse_sensor_data(encoder.reply)
+            current = state["encoder"]
+
+             # limit in between
+            if prev - current >= threshold:
+                counts += N - prev + current
+            # limit not in between
+            elif 0 <= current - prev <= threshold:
+                counts += current - prev
+            # limit in between
+            elif 0 <= prev - current <= threshold:
+                counts -= prev - current
+            elif current - prev >= threshold:
+                counts -= N - current + prev
+
+            a[j,i] = counts/N * 2 * np.pi
+            prev = current
+
+            sleep(k)
+
+except KeyboardInterrupt:
+    print('Disabled by interrupt')
+
+np.savetxt("./data/3.1.txt", a, delimiter=",")
+
+#%%
+
+fig, axs = plt.subplots(1,3, figsize=(18,6))
+
+for i,ax in enumerate(axs):
+    d = dts[i]
+    vs = np.gradient(a[i], d)
+    ts = np.linspace(0, n * d, n)
+    ax.plot(ts, vs)
+    ax.set_xlabel("t [s]")
+    ax.set_ylabel("$\\dot{\\theta}$ [$\\frac{rad}{s}$]")   
+    ax.set_title(f"sampling time = {int(d*1000)} ms")
+
+fig.tight_layout(pad=3.0)
+
+plt.savefig("./plots/3.1.png")
+plt.show()
