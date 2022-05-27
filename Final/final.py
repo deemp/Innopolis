fe16813eaca4554b683b5f96282fc61c7805f478
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-05-27 17:03:05 +0300

    add plots

--- Final/final.py
+++ Final/final.py
@@ -2,143 +2,179 @@
 
 from cProfile import label
 from sympy import *
+import matplotlib.pyplot as plt
+plt.rcParams['font.size'] = '20'
 
-t, a, l1, l2, w, M1, M2, g = symbols('t a l_{1} l_{2} \\omega M_{1} M_{2} g')
-phi_s, dphi_s, ddphi_s = symbols('\\varphi \\dot{\\varphi} \\ddot{\\varphi}',real=True,cls=Function)
+t, a, l, k, m, g, nu = symbols('t a l k m g \\nu')
+phi_s, dphi_s, ddphi_s, nu_s = symbols('\\varphi \\dot{\\varphi} \\ddot{\\varphi} \\nu',real=True,cls=Function)
 phi = phi_s(t)
 dphi = dphi_s(t)
-ddphi = ddphi_s(t)
+nu = nu_s(t)
 
 
-x1 = Rational(0)
-y1 = - a * cos(w * t) - l1
+#%%
 
-x2 = l2 * sin(phi)
-y2 = - a * cos(w * t) - l2 * cos(phi)
+Sy = a * cos(nu * t)
 
-dx1 = x1.diff(t)
-dy1 = y1.diff(t)
+#%%
+Ft = -m * Sy.diff(t).diff(t)
+dnu, ddnu = symbols('\\dot{\\nu} \\ddot{\\nu}')
+# Ft_nu = Ft
+Ft_nu = Ft.subs({Derivative(nu, t): dnu, Derivative(nu, t): ddnu})
+#%%
 
-dx2 = x2.diff(t)
-dy2 = y2.diff(t)
+Ft_nu
+print(Ft_nu)
+#%%
+ddphi_nu = ddphi_s(t)
+ks = solve(-(Ft_nu + m * g) * l * sin(phi) - k * dphi - m * l ** 2 * ddphi_nu, ddphi_nu)
 
-Epot1 = M1 * y1 * g
-Epot2 = M2 * y2 * g
+#%%
+ks[0]
+#%%
 
-Ekin1 = Rational(1, 2) * M1 * (dx1 ** 2 + dy1** 2)
-Ekin2 = Rational(1, 2) * M2 * (dx2 ** 2 + dy2 ** 2)
+# ddphi = - ((a * nu ** 2 * cos(nu * t) + g) * sin(phi)) / l - k * dphi / (m * l**2)
+ddphi = ks[0]
+#%%
+ddphi
+#%%
+print(latex(ddphi))
 
-L = Ekin1 + Ekin2 - (Epot1 + Epot2)
-L = L.subs([(Derivative(phi,t), dphi)]).expand().simplify()
-L
-# print(latex(L))
-
-# %%
-e1 = L.diff(dphi)
-print(latex(e1))
-# %%
-e2 = L.diff(phi)
-print(latex(e2))
-# %%
-e3 = e1.diff(t).subs({Derivative(phi,t): dphi,Derivative(dphi,t): ddphi}).expand().simplify()
-print(latex(e3))
-# %%
-
-e4 = solve((e3 - e2).simplify(), ddphi)[0].simplify().expand()
-ddphi_sym = e4
-ddphi_sym
-# print(latex(e4))
 #%%
 
 x_sym = phi, dphi
 # %%
-f_sym = Matrix([dphi, e4])
+f_sym = Matrix([dphi, ddphi])
 f_sym
 # %%
 
 Jx_sym = f_sym.jacobian(x_sym)
 Jx_sym
+
+#%%
+print(latex(Jx_sym))
 # %%
 
-Ju_sym = f_sym.jacobian([w])
+Ju_sym = f_sym.jacobian([nu])
 Ju_sym
-# %%
 
-# nu, amplitude
-u_d = {w: 100, a: 0.01}
+#%%
+
+print(latex(Ju_sym))
+
+#%% 
+u_d = {a: 0.1, nu: 120}
 x_d = {phi: pi, dphi: 0}
-subs = u_d | x_d
-
+subs_d = u_d | x_d
 #%%
 
-A_sym = Jx_sym.subs(subs)
+A_sym = Jx_sym.subs(subs_d)
 A_sym
+#%%
+print(latex(A_sym))
 # %%
 
-B_sym = Ju_sym.subs(subs)
+B_sym = Ju_sym.subs(subs_d)
 B_sym
-
+print(latex(B_sym))
 
 #%%
 
+m_v = 1
+l_v = 1
+params = {l: l_v, g: 9.8, m: m_v, k: m_v * l **2}
+subs = u_d | x_d | params
 
+A_num_linear = lambdify([t, ddnu],A_sym.subs(subs), 'numpy')
+#%%
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
+
 def pendulum_linearized(x, t, A_num):
     return A_num(t).dot(x)
 
-def calc(sys, A_num):
+def calc(sys, A_num, label):
     t = np.linspace(0, 10, 100000)
-    y0 = np.array([np.pi, 0])
+    y0 = np.array([3 * np.pi / 4, 0])
+    print(A_num(3))
     ys = odeint(func=sys, y0=y0, t=t, args=(A_num,))
     pi2 = np.pi * 2
     md = lambda x: np.fmod(np.fmod(x, pi2) + pi2, pi2)
     md1 = lambda x: np.fmod(x, pi2)
+    id = lambda x : x
     fig, ax = plt.subplots(figsize=(20,10))
-    ax.scatter(t, md(ys[:,0]), label="$\\varphi(t)$", s=3)
-    # ax.plot(t, md(ys[:,0]), label="$\\varphi(t)$")
+    fig.suptitle(f"$\\nu$ = {label}")
+    f = id
+    # ax.scatter(t, id(ys[:,0]), label="$\\varphi(t)$", s=3)
+    ax.plot(t, f(ys[:,0]), label="$\\varphi(t)$")
     # plt.plot(t, ys[:,1], label="$\\dot{\\varphi}(t)$")
     ax.legend()
     plt.plot()
 
-
-
-l2g = {l2: 0.01, g: 9.8}
 #%%
-A_num = lambdify(t, A_sym.subs(l2g), 'numpy')
-# calc(sys=pendulum_linearized, A_num=A_num)
+nu_sin = sin(t).diff(t).diff(t)
+nu_sin
+
+#%%
+print(latex(A_sym.subs(subs| {ddnu: nu_sin})))
+#%%
+A_sin = lambdify(t,A_sym.subs(subs | {ddnu: nu_sin}), 'numpy')
+calc(sys=pendulum_linearized, A_num=A_sin, label=f"${latex(sin(t))}$")
+
+#%%
+nu_constant = Rational(120).diff(t).diff(t)
+nu_constant
+#%%
+A_const = lambdify(t,A_sym.subs(subs | {ddnu: nu_constant}), 'numpy')
+calc(sys=pendulum_linearized, A_num=A_const, label=f"${latex(120)}$")
+
+#%%
+nu_inv = ((t**2.5+3)).diff(t).diff(t)
+nu_inv
+
+#%%
+print(latex(A_sym.subs(subs | {ddnu: nu_inv})))
+
+#%%
+A_inv = lambdify(t, A_sym.subs(subs | {ddnu: nu_inv}), 'numpy')
+calc(sys=pendulum_linearized, A_num=A_inv, label=f"${latex((t**2.5+3))}$")
+
+
 
 # %%
-ddphi_sym.subs(u_d)
-
+ddphi1 = ddphi.subs(u_d | params | {ddnu: 0, nu: 120})
+#%%
+ddphi1
+# print(latex(ddphi1))
+A_num_true = lambdify([t,phi,dphi], ddphi1, 'numpy')
 #%%
 
-def pendulum(x, t, A_num):
+def pendulum_true(x, t, A_num=A_num_true):
     phi, dphi = x
-    return dphi, A_num(t, phi) - dphi
+    return dphi, A_num(t, phi, dphi)
 
-# def calc1(l2_, g_):
-#     t = np.linspace(0, 5, 300)
-#     y0 = np.array([3 * np.pi / 2, 0.1])
-#     ddphi_num = lambdify(phi, ddphi_sym.subs(u_d | {l2:l2_, g:g_}), 'numpy')
-#     ys = odeint(func=sys, y0=y0, t=t, args=(A_num,))
-#     plt.plot(t, ys)
+def calc1(label):
+    t = np.linspace(0, 10, 100000)
+    y0 = np.array([3 * np.pi / 4, 0])
+    ys = odeint(func=pendulum_true, y0=y0, t=t)
+    pi2 = np.pi * 2
+    md = lambda x: np.fmod(np.fmod(x, pi2) + pi2, pi2)
+    md1 = lambda x: np.fmod(x, pi2)
+    id = lambda x : x
+    fig, ax = plt.subplots(figsize=(20,10))
+    fig.suptitle(f"$\\nu=120$ in the {label}")
+    f = id
+    # ax.scatter(t, id(ys[:,0]), label="$\\varphi(t)$", s=3)
+    ax.plot(t, f(ys[:,0]), label="$\\varphi(t)$")
+    # plt.plot(t, ys[:,1], label="$\\dot{\\varphi}(t)$")
+    ax.legend()
+    plt.plot()
 
-def get_nu(l, g, a):
-    return (sqrt(2 * g * l / a ** 2) + 1)
-
-def get_a(l, g, nu):
-    return sqrt(2 * g * l / nu ** 2) + 0.001
-
-u_d = {a:0.1, w:get_nu(l=l2g[l2], g=l2g[g], a=0.01),}
-ddphi_num = lambdify([t,phi], ddphi_sym.subs(u_d | l2g), 'numpy')
-# ddphi_num(3)
-ddphi_sym.subs(u_d | l2g)
-calc(sys=pendulum, A_num=ddphi_num)
+calc1(label="original nonlinear system")
 
 # %%
 
-print(get_nu(0.1, 9.8, 0.01))
+# print(get_nu(0.1, 9.8, 0.01))
 # %%
